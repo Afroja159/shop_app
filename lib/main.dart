92c91cde6e4874abaa2f5f48d56512d72684d13d
Afroja159 <afrojarahmanbadhon@gmail.com>
2022-04-06 23:58:33 +0600

    My git commit

---
+++ lib/main.dart
@@ -0,0 +1,140 @@
+import 'package:flutter/material.dart';
+import 'package:carousel_pro/carousel_pro.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Homepage(),
+    );
+  }
+}
+
+class Homepage extends StatefulWidget {
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    Widget image_carousel = Container(
+      height: 200.0,
+      child: Carousel(
+        boxFit: BoxFit.cover,
+        images: [
+          AssetImage('images/p1.jpg'),
+          AssetImage('images/p2.jpg'),
+          AssetImage('images/p3.jpg'),
+          AssetImage('images/p4.jpg'),
+        ],
+        autoplay: false,
+        animationCurve: Curves.fastOutSlowIn,
+        animationDuration: Duration(milliseconds: 1000),
+      ),
+    );
+
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0.0,
+        backgroundColor: Colors.red,
+        title: Text("Shop App"),
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(
+                Icons.search,
+                color: Colors.white,
+              ),
+              onPressed: () {}),
+          IconButton(
+              icon: Icon(
+                Icons.shopping_cart,
+                color: Colors.white,
+              ),
+              onPressed: () {}),
+        ],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              accountName: Text("ABCD"),
+              accountEmail: Text("abcd@gmail.com"),
+              currentAccountPicture: GestureDetector(
+                child: CircleAvatar(
+                  backgroundColor: Colors.grey,
+                  child: Icon(
+                    Icons.person,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text("Home Page"),
+                leading: Icon(Icons.home),
+              ),
+            ),
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text("My Account"),
+                leading: Icon(Icons.person),
+              ),
+            ),
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text("My Orders"),
+                leading: Icon(Icons.shopping_basket),
+              ),
+            ),
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text("Categoris"),
+                leading: Icon(Icons.dashboard),
+              ),
+            ),
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text("Favourites"),
+                leading: Icon(Icons.favorite),
+              ),
+            ),
+            Divider(),
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text("Settings"),
+                leading: Icon(
+                  Icons.settings,
+                  color: Colors.blueAccent,
+                ),
+              ),
+            ),
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text("About"),
+                leading: Icon(
+                  Icons.help,
+                  color: Colors.green,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      body: ListView(
+        children: <Widget>[image_carousel],
+      ),
+    );
+  }
+}
